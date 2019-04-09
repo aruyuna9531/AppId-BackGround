6ea0c31861bf20e966ba7c0ddeeb6f4a7ef0a7a1
yuna <33777205+aruyuna9531@users.noreply.github.com>
2019-04-09 17:01:25 +0800

    路由器抓包代码

---
+++ general.h
@@ -0,0 +1,36 @@
+#ifndef _GENERAL
+#define _GENERAL
+
+
+#include<arpa/inet.h>
+#include<ctype.h>
+#include<errno.h>
+#include<getopt.h>
+#include<math.h>
+#include<memory.h>
+#include<netdb.h>
+#include<netinet/in.h>
+#include<pcap.h>
+#include<pthread.h>
+#include<signal.h>
+#include<stdarg.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<syslog.h>
+#include<sys/socket.h>
+#include<sys/time.h>
+#include<sys/types.h>
+#include<sys/select.h>
+#include<termios.h>
+#include<time.h>
+#include<unistd.h>
+
+#ifdef _Cpp_head
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#endif //_Cpp_head
+
+#endif //_GENERAL
